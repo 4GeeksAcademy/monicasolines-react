16116036346e2830d6a711f5039404d7d633e778
Monica Solines <160787107+monicasolines@users.noreply.github.com>
2024-08-16 19:42:13 +0000

    feat: home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,11 +2,25 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./Navbar.jsx";
+import { Jumbotron } from "./Jumbotron.jsx";
+import { Card } from "./Card.jsx";
+import { Footer } from "./Footer.jsx";
 
 //create your first component
 const Home = () => {
+
 	return (
 		<div className="text-center">
+			<Navbar />
+			<Jumbotron />
+			<div className="row row-cols-1 row-cols-md-3 g-4">
+				<Card /> 
+				<Card />
+				<Card />
+				<Card />
+			</div>
+			
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
@@ -19,6 +33,7 @@ const Home = () => {
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
+			<Footer />
 		</div>
 	);
 };
