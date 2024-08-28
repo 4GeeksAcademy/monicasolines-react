5d4318027b408adc47598307043c628966d7a47a
Monica Solines <160787107+monicasolines@users.noreply.github.com>
2024-08-28 16:42:45 +0000

    footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -7,11 +7,9 @@ export const Footer = () => {
 
     return (
         <div class="bg-dark text-white text-center">
-            <div class="container p-4">
+            <div class="container p-4 text-center">
                 <div class="row">
-                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                        <p> {items.title} </p>
-                    </div>
+                    <p className="mx-auto"> {items.title} </p>
                 </div>
             </div>
         </div>
